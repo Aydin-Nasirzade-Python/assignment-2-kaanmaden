1734567e6771f2f555a02f469c5f9729eb277582
Kaan Maden <160100743+kaanmaden@users.noreply.github.com>
2024-04-06 02:05:13 +0400

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -1,15 +1,12 @@
 def main():
-  month = input("Enter name of the month [ex. June]: ")
-  day = int(input("Enter the day [ex. 5]: "))
+  letter = input("Enter a letter of the alphabet: ")
 
-  if (day >= 20 and month == "March") or (day < 21 and month == "June") or (month == "April" or month == "May"):
-      print(f"{month} {day} is in Spring")
-  elif (day >= 21 and month == "June") or (day < 22 and month == "September") or (month == "July" or month == "August"):
-      print(f"{month} {day} is in Summer")
-  elif (day >= 22 and month == "September") or (day < 21 and month == "December") or (month == "October" or month == "November"):
-      print(f"{month} {day} is in Fall")
+  if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u":
+      print("Entered alphabet is a vowel!")
+  elif letter == "q" or letter == "w" or letter == "r" or letter == "t" or letter == "p" or letter == "s" or letter == "d" or letter == "f" or letter == "g" or letter == "h" or letter == "j" or letter == "k" or letter == "l" or letter == "z" or letter == "x" or letter == "c" or letter == "v" or letter == "b" or letter == "n" or letter == "m":
+      print("Entered alphabet is a consonant!")
   else:
-      print(f"{month} {day} is in Winter")
+      print("Sometimes it is a vowel, and sometimes it is a consonant!")
   pass
 
 if __name__ == "__main__":
