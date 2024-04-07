0594cc7f47618470a21a7657830ccb3c502981b9
Kaan Maden <160100743+kaanmaden@users.noreply.github.com>
2024-04-07 13:37:18 +0400

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -1,12 +1,12 @@
 def main():
     letter=input("Enter a letter of the alphabet: ")
-    if letter=="a" or letter=="e" or letter=="i" or letter=="o" or letter=="u":
+    if letter=="a" or letter=="e" or letter=="i" or\
+       letter=="o" or letter=="u":
         print("Entered alphabet is a vowel!")
     elif letter=="y":
         print("Sometimes it is a vowel, and sometimes it is a consonant!")
     else:
         print("Entered alphabet is a consonant!")
-
   pass
 
 if __name__ == "__main__":
