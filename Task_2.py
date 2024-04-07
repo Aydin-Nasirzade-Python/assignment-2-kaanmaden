716208d650949432ff30bb5033d05cd11a208c47
Kaan Maden <160100743+kaanmaden@users.noreply.github.com>
2024-04-07 13:42:50 +0400

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -1,15 +1,16 @@
 def main():
   month = input("Enter name of the month [ex. June]: ")
   day = int(input("Enter the day [ex. 5]: "))
-
-  if (day >= 20 and month == "March") or (day < 21 and month == "June") or (month == "April" or month == "May"):
+  if ((month == "April" or month == "May") and 1 <= day <= 31) or ((month == "March") and 20 <= day <= 31) or ((month == "June") and 1 <= day < 21):
       print(f"{month} {day} is in Spring")
-  elif (day >= 21 and month == "June") or (day < 22 and month == "September") or (month == "July" or month == "August"):
+  elif ((month == "July" or month == "August") and 1 <= day <= 31) or ((month == "June") and 21 <= day <= 30) or ((month == "September") and 1 <= day < 22):
       print(f"{month} {day} is in Summer")
-  elif (day >= 22 and month == "September") or (day < 21 and month == "December") or (month == "October" or month == "November"):
+  elif ((month == "October" or month == "November") and 1 <= day <= 31) or ((month == "September") and 22 <= day <= 30) or ((month == "December") and 1 <= day < 21):
       print(f"{month} {day} is in Fall")
-  else:
+  elif ((month == "January" or month == "February") and 1 <= day <= 31) or (month == "March" and 1 <= day < 20) or (month == "December" and 21 <= day <= 31):
       print(f"{month} {day} is in Winter")
+  else:
+      print("Invalid")
   pass
 
 if __name__ == "__main__":
