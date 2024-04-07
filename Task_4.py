86f6cb2e7f2656a4a146c22a53fac57a1980d1ef
Kaan Maden <160100743+kaanmaden@users.noreply.github.com>
2024-04-07 13:33:05 +0400

    Update Task_4.py

--- Task_4.py
+++ Task_4.py
@@ -1,34 +1,31 @@
-
 def main():
-  year = int(input("Enter the year [ex. 2021]: "))
-
-  if (year - 2000) % 12 == 0 :
-      print(f"{year} is the year of the Dragon")
-  elif (year - 2000) % 12 == 1 :
-      print(f"{year} is the year of the Snake")
-  elif (year - 2000) % 12 == 2 :
-      print(f"{year} is the year of the Horse")
-  elif (year - 2000) % 12 == 3 :
-      print(f"{year} is the year of the Sheep")
-  elif (year - 2000) % 12 == 4 :
-      print(f"{year} is the year of the Monkey")
-  elif (year - 2000) % 12 == 5 :
-      print(f"{year} is the year of the Rooster")
-  elif (year - 2000) % 12 == 6 :
-      print(f"{year} is the year of the Dog")
-  elif (year - 2000) % 12 == 7 :
-      print(f"{year} is the year of the Pig")
-  elif (year - 2000) % 12 == 8 :
-      print(f"{year} is the year of the Rat")
-  elif (year - 2000) % 12 == 9 :
-      print(f"{year} is the year of the Ox")
-  elif (year - 2000) % 12 == 10 :
-      print(f"{year} is the year of the Tiger")
-  elif (year - 2000) % 12 == 11 :
-      print(f"{year} is the year of the Hare")
-  elif year < 0 :
-      print("Invalid year!")
-  else :
+  y=int(input("Enter the year [ex. 2021]: "))
+  if y>=0:
+      if y%12==8:
+          print(f"{y} is the year of the Dragon")
+      elif y%12==9:
+          print(f"{y} is the year of the Snake")
+      elif y%12==10:
+          print(f"{y} is the year of the Horse")
+      elif y%12==11:
+          print(f"{y} is the year of the Sheep")
+      elif y%12==0:
+          print(f"{y} is the year of the Monkey")
+      elif y%12==1:
+          print(f"{y} is the year of the Rooster")
+      elif y%12==2:
+          print(f"{y} is the year of the Dog")
+      elif y%12==3:
+          print(f"{y} is the year of the Pig")
+      elif y%12==4:
+          print(f"{y} is the year of the Rat")
+      elif y%12==5:
+          print(f"{y} is the year of the Ox")
+      elif y%12==6:
+          print(f"{y} is the year of the Tiger")
+      elif y%12==7:
+          print(f"{y} is the year of the Hare")
+  else:
       print("Invalid year!")
   pass
 
